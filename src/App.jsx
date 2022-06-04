2e057ea6586f90717edcb7352057bc762e38c47d
Leancen22 <leandromesaprofesional@gmail.com>
2022-06-03 23:53:39 -0300

    todo list reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-import CounterApp from "./Components/01-useState/CounterApp"
-import CounterWithCustomHook from "./Components/01-useState/CounterWithCustomHook"
-import FromWithCustomHook from "./Components/02-useEffect/FormWithCustomHook"
-import SimpleForm from "./Components/02-useEffect/SimpleForm"
-import MultipleCustomHooks from "./Components/03-examples/MultipleCustomHooks"
-import FocusScreen from "./Components/04-useRef/FocusScreen"
-import RealExapleRef from "./Components/04-useRef/RealExapleRef"
-import Layout from "./Components/05-useLayoutDefect/Layout"
-import CallBackHook from "./Components/06-Memos/CallBackHook"
-import MemoHook from "./Components/06-Memos/MemoHook"
-import Memorize from "./Components/06-Memos/Memorize"
 import HookApp from "./Components/HookApp"
 
 function App() {
@@ -16,26 +5,6 @@ function App() {
   return (
     <>
       <HookApp/>
-      <CounterApp/>
-      <CounterWithCustomHook/>
-      <hr/>
-      <SimpleForm/>
-      <FromWithCustomHook/>
-      <hr/>
-      <hr/>
-      <MultipleCustomHooks/>
-      <hr/>
-      <FocusScreen/>
-      <hr/>
-      <RealExapleRef/>
-      <hr/>
-      <Layout/>
-      <hr/>
-      <Memorize/>
-      <hr/>
-      <MemoHook/>
-      <hr/>
-      <CallBackHook/>
     </>
   )
 }
--- src/Components/HookApp.jsx
+++ src/Components/HookApp.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import TodoApp from './07-useReducer/TodoApp'
+import CounterApp from "./01-useState/CounterApp"
+import CounterWithCustomHook from "./01-useState/CounterWithCustomHook"
+import FromWithCustomHook from "./02-useEffect/FormWithCustomHook"
+import SimpleForm from "./02-useEffect/SimpleForm"
+import MultipleCustomHooks from "./03-examples/MultipleCustomHooks"
+import FocusScreen from "./04-useRef/FocusScreen"
+import RealExapleRef from "./04-useRef/RealExapleRef"
+import Layout from "./05-useLayoutDefect/Layout"
+import CallBackHook from "./06-Memos/CallBackHook"
+import MemoHook from "./06-Memos/MemoHook"
+import Memorize from "./06-Memos/Memorize"
 
 const HookApp = () => {
   return (
-    <div>HookApp</div>
+    <div>
+      <TodoApp/>
+      <hr/>
+      <CounterApp/>
+      <CounterWithCustomHook/>
+      <hr/>
+      <SimpleForm/>
+      <FromWithCustomHook/>
+      <hr/>
+      <hr/>
+      <MultipleCustomHooks/>
+      <hr/>
+      <FocusScreen/>
+      <hr/>
+      <RealExapleRef/>
+      <hr/>
+      <Layout/>
+      <hr/>
+      <Memorize/>
+      <hr/>
+      <MemoHook/>
+      <hr/>
+      <CallBackHook/>
+      <hr/>
+
+    </div>
   )
 }
 
